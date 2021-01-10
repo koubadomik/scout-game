e4aa796f7fda4c74fc8bd63f7f6ce0d1267f992d
Dominik Kouba <31382955+koubadomik@users.noreply.github.com>
2021-01-10 14:47:59 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,1 @@
-const mapping = {
-	"Hello": "Ahoj :)",
-  "Bum bác": "<3"
-};
-let submit_button = document.querySelector("input[type=submit]");
-submit_button.addEventListener("click", event => {
-	let text_field = document.querySelector(".answer")
-  let written = document.querySelector("#name").value
-  let answer = mapping[written]
-  if(answer === undefined){
-  	text_field.innerHTML = "Takové slovo tam nebylo :D"
-  }
-  else{
-  	text_field.innerHTML = answer
-  }
-  event.preventDefault();
-});
-console.log(submit_button);
+
